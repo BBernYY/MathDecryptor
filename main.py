a94dfc76410157d3682338a718589552a22b0bdb
BBernYY <66414852+BBernYY@users.noreply.github.com>
2022-11-10 16:56:43 +0100

    single-layer functionality

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
-def main():
-    import utils # imports my customized utils module, with a test and timing function. https://GitHub.com/BBernYY/FancyCoding
-    print("hello world!")
+import math
+def get_simple_theory(a, n):
+    
+    results = []
+    # Addition - Subtraction
+    results.append(f"a + {n - a}")
+    results.append(f"a - {a - n}")
+    # Multiplication - Division
+    results.append(f"a / {a / n}")
+    results.append(f"a * {1/(a/n)}")
+    # Exponent - Square-root
+    results.append(f"a**{math.log(n)/math.log(a)}")
+
+    return results
+def get_theories(df):
+    results = []
+    for i in df:
+        for ii in get_simple_theory(i[0], i[1]):
+            results.append(ii)
+    return results
+
+def check_theory(theory, a, b):
+    return eval(theory.replace("a", str(a))) == b
+
+def main(df):
+    
+    theories = get_theories(df)
+    for i in theories:
+        if theories.count(i) >= len(df):
+            theory = i
+            break
+    
+    return theory
+
 
 
 
 if __name__ == '__main__': # checks if the code is ran as a file
-    main() # starts the main function
+    print(main([
+        [2, 16],
+        [88, 59969536]
+    ]))
