c36993a21c7af1d7253fa477e2284eb0f0dfd855
BBernYY <66414852+BBernYY@users.noreply.github.com>
2022-11-11 15:13:28 +0100

    Changed way of input

--- main.py
+++ main.py
@@ -1,42 +1,65 @@
 import math
+from collections import Counter
 def get_simple_theory(a, n):
     
-    results = []
+    results = {}
     # Addition - Subtraction
-    results.append(f"a + {n - a}")
-    results.append(f"a - {a - n}")
+    results["+"] = a + n
+    results["-"] = a - n
     # Multiplication - Division
-    results.append(f"a / {a / n}")
-    results.append(f"a * {1/(a/n)}")
+    results["*"] = 1/(a/n)
+    results["/"] = a/n
     # Exponent - Square-root
-    results.append(f"a**{math.log(n)/math.log(a)}")
+    if a == 1:
+        results["**"] = math.log(n)/math.log(a)
+    else:
+        results["**"] = 1
 
     return results
+def format_results(results):
+    out = []
+    for i in results:
+        for k, v in i.items():
+            out.append({
+                "type": k,
+                "num": v,
+                "format": f"a {k} {v}"
+                })
+    return out
 def get_theories(df):
     results = []
     for i in df:
-        for ii in get_simple_theory(i[0], i[1]):
-            results.append(ii)
+        results.append(get_simple_theory(i[0], i[1]))
     return results
 
-def check_theory(theory, a, b):
-    return eval(theory.replace("a", str(a))) == b
+def sort_theories(values, total_list_length):
+    rangedlist = []
+    for i in values:
+        rangedlist.append(i['format'])
+    result = [item for items, c in Counter(rangedlist).most_common() for item in [items] * c]
+    out = {}
+    for i in result:
+        if i in out:
+            out[i] += 1
+        else:
+            out[i] = 1
+    for k, v in out.items():
+        out[k] = round(v/total_list_length, 3)*100
+    return out
+    
+
+
 
 def main(df):
-    
-    theories = get_theories(df)
-    for i in theories:
-        if theories.count(i) >= len(df):
-            theory = i
-            break
-    
-    return theory
+    output = []
+    theories = format_results(get_theories(df))
+    return sort_theories(theories, len(df))
 
 
 
 
 if __name__ == '__main__': # checks if the code is ran as a file
     print(main([
-        [2, 16],
-        [88, 59969536]
+        [3, 6],
+        [6, 12]
     ]))
